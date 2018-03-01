c8d989ed867c3b0bcac20352f180ad3cc11edf0b
james <j3k.2004@gmail.com>
2018-03-01 17:08:18 +1300

    changes

--- Bash/provision/ubuntu/web-bootstrapper.sh
+++ Bash/provision/ubuntu/web-bootstrapper.sh
@@ -3,18 +3,20 @@
 cd "${HOME}" ; mkdir src ; cd src ; mkdir _provision ; cd _provision
 # ${HOME}/src/_provision
 provisionpuburl="https://github.com/jameswilliamknight/scripts.pub/raw/master/Bash/provision"
+
 wget "${provisionpuburl}/ubuntu/github-bootstrap.sh"
+chmod +x github-bootstrap.sh
+
 wget "${provisionpuburl}/add-github-pubkey.sh"
-#mkdir ubuntu ; cd ubuntu
-# ${HOME}/src/_provision/ubuntu
+chmod +x add-github-pubkey.sh
+
 wget "${provisionpuburl}/ubuntu/bootstrapper.sh"
-wget "${provisionpuburl}/ubuntu/start.md"
+chmod +x bootstrapper.sh
 
 wget "${provisionpuburl}/xclip-git-ssh-keys.sh"
+chmod +x xclip-git-ssh-keys.sh
 
-chmod +x github-bootstrap.sh
-chmod +x add-github-pubkey.sh
-chmod +x bootstrapper.sh
-#
-# TODO: Test. Also, don't run dodgy scripts you've found on the internet, you imbecile.
+wget "${provisionpuburl}/ubuntu/start.md"
+
+# bazinga
 . bootstrapper.sh
