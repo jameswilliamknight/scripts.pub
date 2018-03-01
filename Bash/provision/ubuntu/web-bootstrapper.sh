051197ff04637e3f15b722710d94ec953023b27d
james <j3k.2004@gmail.com>
2018-03-01 20:45:48 +1300

    changes

--- Bash/provision/ubuntu/web-bootstrapper.sh
+++ Bash/provision/ubuntu/web-bootstrapper.sh
@@ -6,34 +6,34 @@ cd "${HOME}" ; mkdir src ; cd src ; mkdir _provision ; cd _provision
 # ${HOME}/src/_provision
 urlbase="https://github.com/jameswilliamknight/scripts.pub/raw/master/Bash/provision"
 
-wget "${urlbase}/logger.sh" --no-cache
+wget -q "${urlbase}/logger.sh" --no-cache
 chmod +x logger.sh
 
-wget "${urlbase}/github-bootstrap.sh" --no-cache
+wget -q "${urlbase}/github-bootstrap.sh" --no-cache
 chmod +x github-bootstrap.sh
 
-wget "${urlbase}/ubuntu/params.sh" --no-cache
+wget -q "${urlbase}/ubuntu/params.sh" --no-cache
 chmod +x params.sh
 
-wget "${urlbase}/add-github-pubkey.sh" --no-cache
+wget -q "${urlbase}/add-github-pubkey.sh" --no-cache
 chmod +x add-github-pubkey.sh
 
-wget "${urlbase}/ubuntu/bootstrapper.sh" --no-cache
+wget -q "${urlbase}/ubuntu/bootstrapper.sh" --no-cache
 chmod +x bootstrapper.sh
 
-wget "${urlbase}/setup-ssh-keys.sh" --no-cache
+wget -q "${urlbase}/setup-ssh-keys.sh" --no-cache
 chmod +x xclip-git-ssh-keys.sh
 
-wget "${urlbase}/git.sh" --no-cache
+wget -q "${urlbase}/git.sh" --no-cache
 chmod +x git.sh
 
-wget "${urlbase}/curl.sh" --no-cache
+wget -q "${urlbase}/curl.sh" --no-cache
 chmod +x curl.sh
 
-wget "${urlbase}/snap-expect.sh" --no-cache
+wget -q "${urlbase}/snap-expect.sh" --no-cache
 chmod +x snap-expect.sh
 
-wget "${urlbase}/ubuntu/start.md" --no-cache
+wget -q "${urlbase}/ubuntu/start.md" --no-cache
 
 # bazinga
 . bootstrapper.sh
