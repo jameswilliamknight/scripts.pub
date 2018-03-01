81eb7cb46ab91eed4af278291fe705e52d057abe
james <j3k.2004@gmail.com>
2018-03-01 17:18:25 +1300

    updates

--- Bash/provision/ubuntu/web-bootstrapper.sh
+++ Bash/provision/ubuntu/web-bootstrapper.sh
@@ -2,21 +2,21 @@
 
 cd "${HOME}" ; mkdir src ; cd src ; mkdir _provision ; cd _provision
 # ${HOME}/src/_provision
-provisionpuburl="https://github.com/jameswilliamknight/scripts.pub/raw/master/Bash/provision"
+urlbase="https://github.com/jameswilliamknight/scripts.pub/raw/master/Bash/provision"
 
-wget "${provisionpuburl}/ubuntu/github-bootstrap.sh"
+wget "${urlbase}/ubuntu/github-bootstrap.sh"
 chmod +x github-bootstrap.sh
 
-wget "${provisionpuburl}/add-github-pubkey.sh"
+wget "${urlbase}/add-github-pubkey.sh"
 chmod +x add-github-pubkey.sh
 
-wget "${provisionpuburl}/ubuntu/bootstrapper.sh"
+wget "${urlbase}/ubuntu/bootstrapper.sh"
 chmod +x bootstrapper.sh
 
-wget "${provisionpuburl}/xclip-git-ssh-keys.sh"
+wget "${urlbase}/xclip-git-ssh-keys.sh"
 chmod +x xclip-git-ssh-keys.sh
 
-wget "${provisionpuburl}/ubuntu/start.md"
+wget "${urlbase}/ubuntu/start.md"
 
 # bazinga
 . bootstrapper.sh
