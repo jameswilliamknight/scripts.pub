3835e9144599f1c8cdf1539c9f39d139d3cc78d7
james <j3k.2004@gmail.com>
2018-03-01 23:37:34 +1300

    changes

--- Bash/provision/generate-ssh-key.sh
+++ Bash/provision/generate-ssh-key.sh
@@ -21,13 +21,15 @@ printf "Date: ${DATE}\nHome: ${HOME}\n\n" >> ~/bootstrap.log
 # Creating RSA Key Pair
 set -x
 
-XYZ=$(expect -c "
-spawn ssh-keygen -t rsa -b 4096 -C \"$email\" -f \"${HOME}/.ssh/id_rsa\"
-expect \"Enter passphrase (empty for no passphrase):\"
-send \"${passphrase}\r\"
-expect \"Enter same passphrase again:\"
-send \"${passphrase}\r\"
-")
+ssh-keygen -t rsa -b 4096 -C "$email" -f "${HOME}/.ssh/id_rsa"
+
+#XYZ=$(expect -c "
+#spawn ssh-keygen -t rsa -b 4096 -C \"$email\" -f \"${HOME}/.ssh/id_rsa\"
+#expect \"Enter passphrase (empty for no passphrase):\"
+#send \"${passphrase}\"
+#expect \"Enter same passphrase again:\"
+#send \"${passphrase}\"
+#")
 
 set +x
 
