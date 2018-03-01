5f28926b45720ff600129bb136ffc71b6b16bf54
james <j3k.2004@gmail.com>
2018-03-01 22:30:35 +1300

    changes

--- Bash/provision/ubuntu/bootstrapper.sh
+++ Bash/provision/ubuntu/bootstrapper.sh
@@ -87,7 +87,7 @@ fi
 . git.sh
 . curl.sh
 . setup-ssh-keys.sh "${global_email}" "${global_pcname}" "${global_passphrase}"
-. add-github-pubkey.sh "${global_pcname}" "${global_githubusername}" "${global_token}" -k
+. add-github-pubkey.sh "${global_pcname}" "${global_githubusername}" "${global_token}"
 
 logthis "finished - bootstrapper.sh"
 return 0;
