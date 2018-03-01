b647a5526241891ff933f76c3e3588b0445c631b
james <j3k.2004@gmail.com>
2018-03-01 18:55:46 +1300

    updates

--- Bash/provision/generate-ssh-key.sh
+++ Bash/provision/generate-ssh-key.sh
@@ -4,6 +4,17 @@
 #
 #   generates a user ssh key via an experimental process. Extracted to test.
 
+P="expect"
+for P; do
+    dpkg -s "$P" >/dev/null 2>&1 && {
+        # echo "$P is installed."
+        exit 0;
+    } || {
+        # echo "$P is not installed."
+        exit 1;
+    }
+done
+
 email=$1
 passphrase=$2
 
@@ -11,7 +22,7 @@ DATE=`date '+%Y-%m-%d %H:%M:%S'`
 printf "Date: ${DATE}\nHome: ${HOME}\n\n" >> ~/bootstrap.log
 
 # Creating RSA Key Pair
-set -x
+#set -x
 XYZ=$(expect -c "
 spawn ssh-keygen -t rsa -b 4096 -C \"$email\" -f \"${HOME}/.ssh/id_rsa\"
 expect \"Enter passphrase (empty for no passphrase):\"
@@ -19,3 +30,4 @@ send \"${passphrase}\r\"
 expect \"Enter same passphrase again:\"
 send \"${passphrase}\r\"
 ")
+#set +x
