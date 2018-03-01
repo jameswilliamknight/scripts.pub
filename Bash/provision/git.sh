7998ab2b7d8f0f5806ffdb93a3398256a9b2c441
james <j3k.2004@gmail.com>
2018-03-01 23:30:16 +1300

    changes

--- Bash/provision/git.sh
+++ Bash/provision/git.sh
@@ -16,5 +16,5 @@ fi
 . logger.sh ; loggerstarted "scripts.pub\Bash\provision\git.sh"
 
 logthis "installing git-all..."
-sudo apt-get -qq install git-all -y
+sudo apt-get -qq install git-all -y 1> /dev/null
 logthis "finished installing git-all"
