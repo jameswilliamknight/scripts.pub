439014c254d3ba9f26536debaa46653dad953767
James Knight <j3k.2004@gmail.com>
2018-03-10 20:33:18 +1300

    updates

--- Bash/provision/ubuntu/web-bootstrapper.sh
+++ Bash/provision/ubuntu/web-bootstrapper.sh
@@ -8,25 +8,29 @@ cd "${HOME}" ; mkdir src ; cd src ; mkdir _provision ; cd _provision
 sudo apt-get -qq install dos2unix -y
 
 function downloader () {
-    urlbase="https://github.com/jameswilliamknight/scripts.pub/raw/master/Bash/provision"
-    wget -q --no-cache "${urlbase}/$1"
-    dos2unix logger.sh
-    if [ ! -z ${2+x} ] && [ "${2}" != "no-execute" ]; then
-        chmod +x logger.sh
+    #
+    downloadfilename="${1}"
+    #
+    if [ ! -z ${2+x} ]
+    then
+        filename="${2}"
     fi
+    urlbase="https://github.com/jameswilliamknight/scripts.pub/raw/master/Bash/provision"
+    wget -q --no-cache "${urlbase}/${downloadfilename}"
+    dos2unix "${filename}"
+
 }
 
 downloader "logger.sh"
 downloader "generate-ssh-key.sh"
 downloader "github-bootstrap.sh"
-downloader "ubuntu/params.sh"
+downloader "ubuntu/params.sh" "ubuntu/params.sh"
 downloader "add-github-pubkey.sh"
-downloader "ubuntu/bootstrapper.sh"
+downloader "ubuntu/bootstrapper.sh" "bootstrapper.sh"
 downloader "setup-ssh-keys.sh"
 downloader "git.sh"
 downloader "curl.sh"
 downloader "snap-expect.sh"
-downloader "ubuntu/start.md" "no-execute"
 
 # bazinga
 . bootstrapper.sh
